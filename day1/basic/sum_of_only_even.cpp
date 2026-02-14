6a76c6915a36fc9c2cfafbdffa3d091db3744ec5
Habibu Rahaman Ansari <rhabibu71@gmail.com>
2026-02-14 15:42:35 +0545

    add only even

---
+++ day1/basic/sum_of_only_even.cpp
@@ -0,0 +1,13 @@
+//Print only even numbers from 1 to 20
+#include <iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    for(int i=1;i<=20;i++){
+        if(i%2==0){
+            sum=sum+i;
+        }
+    }
+    cout<<"the sum of the even num is:"<<sum<<endl;
+    return 0;  
+}
